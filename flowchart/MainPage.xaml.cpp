b779e948908bbe6d5d9408d4a4aa0e1cf1416e01
uwangg <uwangg@gmail.com>
2016-12-20 13:29:17 +0900

    project creation

---
+++ flowchart/MainPage.xaml.cpp
@@ -0,0 +1,27 @@
+﻿//
+// MainPage.xaml.cpp
+// MainPage 클래스의 구현입니다.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+
+using namespace flowchart;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+// 빈 페이지 항목 템플릿은 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 에 문서화되어 있습니다.
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+}
